a02b08e6ed3ed5951288f040831f85f896fe7e33
LSMJJAng <127660101+LSMJJAng@users.noreply.github.com>
2023-08-07 17:42:45 +0900

    안드로이드 기본 셋팅 업로드

---
+++ android/app/src/main/java/com/example/moHaeng/LocationPermissionActivity.kt
@@ -0,0 +1,98 @@
+package com.example.moHaeng
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Looper
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
+import com.example.moHaeng.databinding.ActivityLocationBinding
+
+open class LocationPermissionActivity : AppCompatActivity() {
+
+    private var mFusedLocationProviderClient: FusedLocationProviderClient? = null
+    private lateinit var mLastLocation: Location
+    private lateinit var mLocationRequest: LocationRequest
+    private val requestPermissionLocation = 10
+
+    private var _binding: ActivityLocationBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityLocationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, KakaoMapsFragment())
+                .commit()
+        }
+
+        binding.button.setOnClickListener {
+            if (checkPermissionForLocation(this)) {
+                startLocationUpdates()
+            }
+        }
+
+        mLocationRequest = LocationRequest.create().apply {
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+    }
+
+    private fun startLocationUpdates() {
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return
+        }
+        Looper.myLooper()?.let {
+            mFusedLocationProviderClient!!.requestLocationUpdates(mLocationRequest, mLocationCallback, it)
+        }
+    }
+
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            locationResult.lastLocation?.let { location ->
+                onLocationChanged(location)
+            }
+        }
+    }
+
+    private fun onLocationChanged(location: Location) {
+        mLastLocation = location
+        binding.text2.text = "위도 : ${mLastLocation.latitude}"
+        binding.text1.text = "경도 : ${mLastLocation.longitude}"
+    }
+
+    private fun checkPermissionForLocation(context: Context): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (context.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                true
+            } else {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), requestPermissionLocation)
+                false
+            }
+        } else {
+            true
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == requestPermissionLocation) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startLocationUpdates()
+            } else {
+                Log.d("ttt", "onRequestPermissionsResult() _ 권한 허용 거부")
+                Toast.makeText(this, "권한이 없어 해당 기능을 실행할 수 없습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
