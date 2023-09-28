4b12e6eacd03e39ff41f5faf712105cb9c7ba033
LSMJJAng <127660101+LSMJJAng@users.noreply.github.com>
2023-09-29 00:28:42 +0900

    HomeActivity 카테고리 랭킹 해당 button 작업

--- android/app/src/main/java/com/example/moHaeng/DetailActivity.kt
+++ android/app/src/main/java/com/example/moHaeng/DetailActivity.kt
@@ -19,7 +19,7 @@ class DetailActivity : AppCompatActivity() {
         setContentView(view)
 
         // RecyclerView 초기화
-        val recyclerView: RecyclerView = binding.RecommendRecyclerView
+        val recyclerView: RecyclerView = binding.recommendRecyclerView
 
         // LinearLayoutManager를 수평 방향으로 설정
         val layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
@@ -30,7 +30,6 @@ class DetailActivity : AppCompatActivity() {
             RecommendItem("상점1", "상품1", "10%", "5,000", "4,500", true),
             RecommendItem("상점2", "상품2", "5%", "10,000", "9,500", true),
             RecommendItem("상점3", "상품3", "0%", "7,000", "7,000", false)
-            // 추가 아이템 계속해서 추가
         )
 
         // RecommendAdapter를 생성하고 RecyclerView에 연결
--- android/app/src/main/java/com/example/moHaeng/HomeActivity.kt
+++ android/app/src/main/java/com/example/moHaeng/HomeActivity.kt
@@ -1,23 +1,77 @@
 package com.example.moHaeng
 
+import android.content.res.Resources
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.moHaeng.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        val categoryItemList = mutableListOf<CategoryItem>()
-        categoryItemList.add(CategoryItem("카테고리1", R.drawable.ic_launcher_background))
-        categoryItemList.add(CategoryItem("카테고리2", R.drawable.ic_launcher_background))
-        val categoryAdapter = CategoryAdapter(categoryItemList)
 
-        val recyclerView: RecyclerView = findViewById(R.id.categoryRecyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = categoryAdapter
+        // RecyclerView 초기화
+        val recyclerView: RecyclerView = binding.categoryRecyclerView
+
+
+
+        val layoutManager = GridLayoutManager(this, 2, GridLayoutManager.HORIZONTAL, false)
+        recyclerView.layoutManager = layoutManager
+
+
+        val categoryList = listOf(
+            CategoryItem("카테고리1", R.drawable.ic_launcher_background),
+            CategoryItem("카테고리2", R.drawable.ic_launcher_background),
+            CategoryItem("카테고리3", R.drawable.ic_launcher_background),
+            CategoryItem("카테고리4", R.drawable.ic_launcher_background),
+            CategoryItem("카테고리5", R.drawable.ic_launcher_background),
+            CategoryItem("카테고리6", R.drawable.ic_launcher_background),
+            CategoryItem("카테고리7", R.drawable.ic_launcher_background),
+            CategoryItem("카테고리8", R.drawable.ic_launcher_background),
+            CategoryItem("카테고리9", R.drawable.ic_launcher_background),
+            CategoryItem("카테고리0", R.drawable.ic_launcher_background)
+        )
+
+        val adapter = CategoryAdapter(categoryList)
+        recyclerView.adapter = adapter
+
+        recyclerView?.run {
+            val spanCount = categoryList.size / 2
+            val space = (Resources.getSystem().displayMetrics.density).toInt()
+            addItemDecoration(GridSpaceItemDecoration(spanCount, space))
+        }
+    }
+}
+// GridLayoutManager의 간격 조절
+class GridSpaceItemDecoration(private val spanCount: Int, private val space: Int) :
+    RecyclerView.ItemDecoration() {
+
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        val position = parent.getChildAdapterPosition(view)     // item position
+        val row = position % 2 + 1      // item row
+
+        // 첫 행 제외하고 상단 여백 추가
+        if (row == 2) {
+            outRect.top = space * 2
+        }
+        // 첫번째 열을 제외하고 좌측 여백 추가
+        if (position >= 2) {
+            outRect.left = space * 8
+        }
     }
 }
